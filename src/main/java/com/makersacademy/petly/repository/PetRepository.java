5084378ea376770ccde06d199b27fb77d672dd1c
LeraSher <LeraSher>
2026-08-26 13:38:06 +0100

    Pet repo updated

--- src/main/java/com/makersacademy/petly/repository/PetRepository.java
+++ src/main/java/com/makersacademy/petly/repository/PetRepository.java
@@ -3,5 +3,8 @@ package com.makersacademy.petly.repository;
 import com.makersacademy.petly.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PetRepository extends CrudRepository<Pet, Long> {
+    List<Pet> findByOwnerId(Long ownerId);
 }
